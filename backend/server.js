da1e00d89a6bfbfea258e188335633ae93124659
Vinit Pandy <vps02002@gmail.com>
2025-12-25 01:24:56 -0800

    Fix import casing

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectDB();
 const expenseRoute = require('./routes/Expense.route');
 const incomeRoute = require('./routes/Income.route');
 const budgetRoute = require('./routes/Budget.route');
-const authRoute = require('./routes/auth.route'); 
+const authRoute = require('./routes/Auth.route'); 
 
 app.use('/api/v1', expenseRoute);
 app.use('/api/v1', incomeRoute);
