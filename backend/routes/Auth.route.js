3ab5d67048df64dedf4bfc9cd8a3614f27e4e0b4
Vinit Pandy <vps02002@gmail.com>
2025-12-25 01:34:41 -0800

    Fix controller import casing

--- backend/routes/Auth.route.js
+++ backend/routes/Auth.route.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { register, login } = require('../controller/auth.controller');
+const { register, login } = require('../controller/Auth.controller');
 
 router.post('/register', register);
 router.post('/login', login);
